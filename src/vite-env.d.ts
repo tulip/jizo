b0b5028c7e387bf732fcbbda2961b11ddf670ab0
Connor Tangney <connor.tangney@tulip.co>
2023-04-17 16:35:08 -0400

    update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ declare global {
   var Watcher: InterfaceObserver;
   var LoadModules: function();
   var InitModules: function(Array<CustomElementConstructor>);
+  var axeApi: AxeApi;
+  var electron: ElectronApi;
 
   interface String {
     toKebabCase(): string;
